05acafa913c11305dd0d74f49d870f3e531a4476
douhongchen <douhongchen781@pingan.com.cn>
2016-09-16 16:42:27 +0800

    add files

---
+++ centos-scala/javaenv.sh
@@ -0,0 +1,6 @@
+### 添加环境变量
+export JAVA_HOME=/opt/java/jdk
+export SCALA_HOME=/opt/java/scala
+export CLASSPATH=$JAVA_HOME/lib/tools.jar:$JAVA_HOME/lib/dt.jar
+export PATH=$JAVA_HOME/bin:$SCALA_HOME/bin:/opt/java/sbt/bin:$PATH
+export LD_LIBRARY_PATH=/opt/lib
