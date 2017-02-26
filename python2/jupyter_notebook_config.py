d2584a497196ea04d1568f1b80f12efea256cad7
douhongchen <douhongchen781@pingan.com.cn>
2017-02-26 13:16:47 +0800

    ok

--- python2/jupyter_notebook_config.py
+++ python2/jupyter_notebook_config.py
@@ -1,4 +1,4 @@
-c.NotebookApp.password = u'sha1:fcc2a2aae267:321760dacba4a4aea41a41580c9cb48e43a8d4f2'
+c.NotebookApp.password = u'sha1:5155ea8edc28:f1bb0c60b54d5ca38b070d273c347271e8e073a3'
 c.NotebookApp.certfile = u'/home/work/.jupyter/mycert.pem'
 c.NotebookApp.ip = '*'
 c.NotebookApp.port = 8888
